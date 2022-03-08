15344321704a9c032979afda7f397337a04797dd
Mrityunjoy Bora <mjborabiz@gmail.com>
2022-03-08 19:19:14 +0530

    Add: Progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,33 @@
 import '../styles/globals.css'
+import ProgressBar from "@badrap/bar-of-progress";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+
+const progress = new ProgressBar({
+  size: 2,
+  color: "#FE595E",
+  className: "z-50",
+  delay: 100,
+});
 
 function MyApp({ Component, pageProps }) {
+
+  const router = useRouter();
+
+  useEffect(() => {
+    console.log(router);
+
+    router.events.on("routeChangeStart", progress.start);
+    router.events.on("routeChangeComplete", progress.finish);
+    router.events.on("routeChangeError", progress.finish);
+    return () => {
+      router.events.off("routeChangeStart", progress.start);
+      router.events.off("routeChangeComplete", progress.finish);
+      router.events.off("routeChangeError", progress.finish);
+    };
+  }, [router]);
+
   return <Component {...pageProps} />
 }
 
